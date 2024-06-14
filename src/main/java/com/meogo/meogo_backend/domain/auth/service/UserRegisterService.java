f19b70ecd3533e92a42ff0f9be1c5e8851c11dad
101 <tmddnjs001133@dsm.hs.kr>
2024-06-14 19:18:10 +0900

    update :: UserRegisterService.java
    유저 아이디 검사 기능 추가 및 리팩토링

--- src/main/java/com/meogo/meogo_backend/domain/auth/service/UserRegisterService.java
+++ src/main/java/com/meogo/meogo_backend/domain/auth/service/UserRegisterService.java
@@ -4,6 +4,7 @@ import com.meogo.meogo_backend.domain.auth.dto.UserRegisterRequest;
 import com.meogo.meogo_backend.domain.auth.usecase.UserRegisterUseCase;
 import com.meogo.meogo_backend.domain.user.model.UserEntity;
 import com.meogo.meogo_backend.domain.user.repository.UserRepository;
+import com.meogo.meogo_backend.global.exception.custom.ExistingUserIdException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,14 +12,20 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserRegisterService implements UserRegisterUseCase {
-    private final PasswordEncoder passwordEncoder;
-    private final UserRepository userRepository;
-
     @Override
     public void register(UserRegisterRequest request) {
-        userRepository.save(
-                createEntity(request)
-        );
+        validateUserId(request.userId());
+        saveUser(request);
+    }
+
+    private void saveUser(UserRegisterRequest request) {
+        userRepository.save(createEntity(request));
+    }
+
+    private void validateUserId(String userId) {
+        if (userRepository.existsByUserId(userId)) {
+            throw ExistingUserIdException.EXCEPTION;
+        }
     }
 
     private UserEntity createEntity(UserRegisterRequest request) {
@@ -31,4 +38,7 @@ public class UserRegisterService implements UserRegisterUseCase {
                 request.enrolledSchool()
         );
     }
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 }
