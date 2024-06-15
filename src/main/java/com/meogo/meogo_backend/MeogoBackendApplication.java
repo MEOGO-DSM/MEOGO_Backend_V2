c145088919725e1c16a82497a72e7820c420ca86
101 <tmddnjs001133@dsm.hs.kr>
2024-06-15 23:18:36 +0900

    update :: Application
    ConfigurationPropertiesScan 어노테이션 추가 및
    EnableWebSecurity 어노테이션 위치 이동
    (SecurityConfig -> MeogoBackendApplication)

--- src/main/java/com/meogo/meogo_backend/MeogoBackendApplication.java
+++ src/main/java/com/meogo/meogo_backend/MeogoBackendApplication.java
@@ -2,8 +2,12 @@ package com.meogo.meogo_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class MeogoBackendApplication {
 
     public static void main(String[] args) {
