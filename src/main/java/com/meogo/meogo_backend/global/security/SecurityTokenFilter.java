9c58c0facc856d134c18ac6986da0e5df19700c2
101 <tmddnjs001133@dsm.hs.kr>
2024-06-16 10:19:42 +0900

    add :: SecurityTokenFilter.java

---
+++ src/main/java/com/meogo/meogo_backend/global/security/SecurityTokenFilter.java
@@ -0,0 +1,39 @@
+package com.meogo.meogo_backend.global.security;
+
+import com.meogo.meogo_backend.global.security.jwt.Tokenizer;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class SecurityTokenFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = extractToken(request.getHeader(HttpHeaders.AUTHORIZATION));
+
+        if (token != null && tokenizer.validate(token)) {
+            Authentication authentication = tokenizer.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractToken(String bearerToken) {
+        if (bearerToken.startsWith(BEARER)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    private static final String BEARER = "bearer ";
+    private final Tokenizer tokenizer;
+}
