77cf79979573bf1dfa9d15b8099a987a1df5a543
ljyo2o9 <yggosxoljy@gmail.com>
2024-06-19 16:31:08 +0900

    update :: ArticleController.java

--- src/main/java/com/meogo/meogo_backend/domain/community/article/controller/ArticleController.java
+++ src/main/java/com/meogo/meogo_backend/domain/community/article/controller/ArticleController.java
@@ -4,6 +4,7 @@ import com.meogo.meogo_backend.domain.community.article.dto.ArticleCreateRequest
 import com.meogo.meogo_backend.domain.community.article.usecase.ArticleUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,7 +15,8 @@ import java.util.List;
 @RequestMapping("/api/community")
 public class ArticleController {
   @PostMapping
-  public void create(@Valid @RequestPart(name = "request") ArticleCreateRequest request, @RequestPart(name = "files") List<MultipartFile> images){
+  @ResponseStatus(HttpStatus.CREATED)
+  public void create(@Valid @RequestPart(name = "request") ArticleCreateRequest request, @RequestPart(name = "images") List<MultipartFile> images){
     articleUseCase.create(request, images);
   }
 
