bc65f70d738ed5b7b8a5bf124c0bfb4af53939b8
ljyo2o9 <yggosxoljy@gmail.com>
2024-06-19 09:03:29 +0900

    add :: ArticleService.java

---
+++ src/main/java/com/meogo/meogo_backend/domain/community/article/service/ArticleService.java
@@ -0,0 +1,36 @@
+package com.meogo.meogo_backend.domain.community.article.service;
+
+import com.meogo.meogo_backend.domain.community.article.dto.ArticleCreateRequest;
+import com.meogo.meogo_backend.domain.community.article.entity.ArticleEntity;
+import com.meogo.meogo_backend.domain.community.article.entity.ArticleModel;
+import com.meogo.meogo_backend.domain.community.article.repository.ArticleRepository;
+import com.meogo.meogo_backend.domain.community.article.usecase.ArticleUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService implements ArticleUseCase {
+
+  @Override
+  public void create(ArticleCreateRequest request, List<MultipartFile> images) {
+    ArticleEntity entity = createEntity(request, images);
+
+    repository.save(entity);
+  }
+
+  private ArticleEntity createEntity(ArticleCreateRequest request, List<MultipartFile> images){
+    return ArticleModel.createArticleEntity(
+            request.title(),
+            request.content(),
+            request.schoolName(),
+            images.stream().map(MultipartFile::getOriginalFilename).toList(),
+            request.tags()
+    );
+  }
+
+  private final ArticleRepository repository;
+}
