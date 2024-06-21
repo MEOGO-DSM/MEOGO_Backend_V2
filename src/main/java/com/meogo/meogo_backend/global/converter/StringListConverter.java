8f9e985100e19ba4024718f4a4e840684fb5cdc7
ljyo2o9 <yggosxoljy@gmail.com>
2024-06-21 11:03:23 +0900

    add :: StringListConverter.java
    DB에 List<String>를 넣으려면 Converter가 필요하다는 것을 알아 이를 추가하였습니다.

---
+++ src/main/java/com/meogo/meogo_backend/global/converter/StringListConverter.java
@@ -0,0 +1,21 @@
+package com.meogo.meogo_backend.global.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Converter
+public class StringListConverter implements AttributeConverter<List<String>, String> {
+  @Override
+  public String convertToDatabaseColumn(List<String> dataList) {
+    return String.join(",", dataList);
+  }
+
+  @Override
+  public List<String> convertToEntityAttribute(String data) {
+    return new ArrayList<>(Arrays.asList(data.split(",")));
+  }
+}
