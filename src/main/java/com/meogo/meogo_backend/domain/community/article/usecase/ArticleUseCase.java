25469c8b54c956f8b8f03d05436fcd84ea9b4273
ljyo2o9 <yggosxoljy@gmail.com>
2024-06-21 23:18:52 +0900

    update :: ArticleUseCase.java

--- src/main/java/com/meogo/meogo_backend/domain/community/article/usecase/ArticleUseCase.java
+++ src/main/java/com/meogo/meogo_backend/domain/community/article/usecase/ArticleUseCase.java
@@ -1,10 +1,13 @@
 package com.meogo.meogo_backend.domain.community.article.usecase;
 
 import com.meogo.meogo_backend.domain.community.article.dto.ArticleCreateRequest;
+import com.meogo.meogo_backend.domain.community.article.dto.ArticleGetResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface ArticleUseCase {
   void create(ArticleCreateRequest request, List<MultipartFile> images);
+
+  List<ArticleGetResponse> findAll();
 }
