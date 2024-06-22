3b9dd8738ccf6f18357438c4c4e120f417add860
ljyo2o9 <yggosxoljy@gmail.com>
2024-06-22 15:28:34 +0900

    update :: ArticleRequest.java
    - Class 명명 변경
    - create에만 사용되는 것이 아니기 때문

--- src/main/java/com/meogo/meogo_backend/domain/community/article/dto/ArticleRequest.java
+++ src/main/java/com/meogo/meogo_backend/domain/community/article/dto/ArticleRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
-public record ArticleCreateRequest(
+public record ArticleRequest(
         @Size(min = 1, max = 20, message = "제목은 1글자 이상 20글자 이하로 입력해주세요")
         String title,
         @Size(min = 1, max = 300, message = "내용은 1글자 이상 300글자 이하로 입력해주세요")
